089e7bf5d888c9b31cafae8f52cbe5cb7bcb341f
kaburia <kaburiaaustin1@gmail.com>
2022-10-08 11:42:34 +0300

    folders

--- data_loader.py
+++ data_loader.py
@@ -13,15 +13,17 @@ def train(labels):
     for label in labels:
         length = int(len(listdir(label)) * 0.8) # choosing 80% of the values
         sample_list = sample(listdir(label), length)
-        # for file in sample_list:
         # creating a training/label folder if they do not exist
         if os.path.exists('Train') is False:
-                os.mkdir('Train')
-                if os.path.exists(f'Train/{label}') is False:
-                    os.mkdir(f'Train/{label}')
-        elif os.path.exists('Train') is True and os.path.exists(f'Train/{label}') is False:
+            os.mkdir('Train')
+            if os.path.exists(f'Train/{label}') is False:
                 os.mkdir(f'Train/{label}')
-                
+        elif os.path.exists('Train') is True and os.path.exists(f'Train/{label}') is False:
+            os.mkdir(f'Train/{label}')
+        for file in sample_list:
+            shutil.copy(f'{label}/{file}', f'Train/{label}')
+        
+                    
                 
 
 
