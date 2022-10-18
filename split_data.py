ae66173517f94f0ec35d78c900561f8051032367
kaburia <kaburiaaustin1@gmail.com>
2022-10-18 03:33:14 +0300

    added path

--- split_data.py
+++ split_data.py
@@ -6,35 +6,39 @@ from os import listdir
 import os
 from random import sample
 
-labels = ['Brown_rust', 'Healthy', 'septoria', 'Yellow_rust']
+
 
 # Building and splitting the dataset
-def split_data(labels):
+def split_data(path):
+    labels = [label for label in listdir(path) if os.path.isdir(label)]
     method = ['Train','Validate', 'Test']
     for meth in method:
         for label in labels:
              # creating a  folder if they do not exist
-        
-            if os.path.exists(f'{meth}') is False:
-                os.mkdir(f'{meth}')
-                if os.path.exists(f'{meth}/{label}') is False:
+            if os.path.isdir(label):
+                if os.path.exists(f'{meth}') is False:
+                    os.mkdir(f'{meth}')
+                    if os.path.exists(f'{meth}/{label}') is False:
+                        os.mkdir(f'{meth}/{label}')
+                elif os.path.exists(f'{meth}') is True and os.path.exists(f'{meth}/{label}') is False:
                     os.mkdir(f'{meth}/{label}')
-            elif os.path.exists(f'{meth}') is True and os.path.exists(f'{meth}/{label}') is False:
-                os.mkdir(f'{meth}/{label}')
 
-            if meth == 'Train':
-                length = int(len(listdir(label)) * 0.8) # choosing 80% of the values
-                sample_list = sample(listdir(label), length)
-                for file in sample_list:
-                    shutil.move(f'{label}/{file}', f'{meth}/{label}') 
-            elif meth == 'Validate':
-                length = int(len(listdir(label)) * 0.5) # choosing 10% of the values
-                sample_list = sample(listdir(label), length)
-                for file in sample_list:
-                    shutil.move(f'{label}/{file}', f'{meth}/{label}') 
-            else:
-                length = int(len(listdir(label))) # choosing the remaining values for test
-                sample_list = sample(listdir(label), length)
-                for file in sample_list:
-                    shutil.move(f'{label}/{file}', f'{meth}/{label}') 
-            
+                if meth == 'Train':
+                    length = int(len(listdir(label)) * 0.8) # choosing 80% of the values
+                    sample_list = sample(listdir(label), length)
+                    for file in sample_list:
+                        shutil.move(f'{label}/{file}', f'{meth}/{label}') 
+                elif meth == 'Validate':
+                    length = int(len(listdir(label)) * 0.5) # choosing 10% of the values
+                    sample_list = sample(listdir(label), length)
+                    for file in sample_list:
+                        shutil.move(f'{label}/{file}', f'{meth}/{label}') 
+                else:
+                    length = int(len(listdir(label))) # choosing the remaining values for test
+                    sample_list = sample(listdir(label), length)
+                    for file in sample_list:
+                        shutil.move(f'{label}/{file}', f'{meth}/{label}') 
+    
+
+
+split_data('YELLOW-RUST-19')
