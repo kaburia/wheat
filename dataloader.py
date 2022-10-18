87df4895ae838c0368eb46fb95e12fe4345fadfa
kaburia <kaburiaaustin1@gmail.com>
2022-10-18 04:15:04 +0300

    data and model set up

--- dataloader.py
+++ dataloader.py
@@ -3,3 +3,37 @@ from torchvision import transforms, datasets
 
 # Loading the split datasets
 # Transforming and augmenting
+def trainloader(trainpath):
+    train_transform = transforms.Compose([transforms.RandomRotation(30),
+                                              transforms.RandomResizedCrop(224),
+                                              transforms.RandomHorizontalFlip(),
+                                              transforms.ToTensor(),
+                                              transforms.Normalize([0.485, 0.456, 0.406],
+                                                                    [0.229, 0.224, 0.225])])
+
+    train_dataset = datasets.ImageFolder(trainpath, transform=train_transform)
+    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=64, shuffle=True)
+    return train_loader
+
+# validation and test transforms
+def transforming():
+    test_transforms  = transforms.Compose((transforms.Resize(255),
+                                      transforms.CenterCrop(224),
+                                      transforms.ToTensor(),
+                                      transforms.Normalize([0.485, 0.456, 0.406],
+                                                           [0.229, 0.224, 0.225])))
+    return test_transforms
+
+# Train validation data
+def val_loader(validate_path):
+    test_transforms  = transforming()
+    val_dataset = datasets.ImageFolder(validate_path, transform=test_transforms)
+    valloader = torch.utils.data.DataLoader(val_dataset, batch_size=64)
+    return valloader
+
+
+def testloader(test_path):
+    test_transforms  = transforming()
+    test_dataset = datasets.ImageFolder(test_path, transform=test_transforms)
+    test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=64)
+    return test_loader
--- train.py
+++ train.py
@@ -2,15 +2,109 @@
 import torch
 from torch import  nn
 from torch.optim import Adam
-import torch.nn.functional as F
 import argparse
 
 # Loading the data
-import dataloader
+from dataloader import trainloader, val_loader
 
 # Building a model 
-import model_loader
+from model_loader import modelling
+
+   
+# Train a model
+def train(img_path, model_name, epochs=1, learning_rate=0.03, device='cpu'):
+    
+    model = modelling(model_name)
+    trainload = trainloader(img_path)
+    val_load = val_loader(img_path)
+    
+    criterion = nn.NLLLoss()
+
+    # Only train the classifier parameters, feature parameters are frozen
+    optimizer = Adam(model.parameters(), lr=learning_rate)
+
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    model.to(device)
+
+    steps = 0
+    running_loss = 0
+    print_every = 5
+
+    print(f'Beginning Training:\nDirectory: {img_path} Model: {model_name}, Epochs: {epochs}, Device: {device}')
+    # epochs = 10
+    steps = 0
+    running_loss = 0
+    print_every = 5
+    for epoch in range(epochs):
+        for inputs, labels in trainload:
+            steps += 1
+            # Move input and label tensors to the default device
+            inputs, labels = inputs.to(device), labels.to(device)
+            
+            logps = model.forward(inputs)
+            loss = criterion(logps, labels)
+            
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            running_loss += loss.item()
+            
+            if steps % print_every == 0:
+                test_loss = 0
+                accuracy = 0
+                model.eval()
+                with torch.no_grad():
+                    for inputs, labels in val_load:
+                        inputs, labels = inputs.to(device), labels.to(device)
+                        logps = model.forward(inputs)
+                        batch_loss = criterion(logps, labels)
+                        
+                        test_loss += batch_loss.item()
+                        
+                        # Calculate accuracy
+                        ps = torch.exp(logps)
+                        top_p, top_class = ps.topk(1, dim=1)
+                        equals = top_class == labels.view(*top_class.shape)
+                        accuracy += torch.mean(equals.type(torch.FloatTensor)).item()
+                        
+                print(f"Epoch {epoch+1}/{epochs}.. "
+                    f"Train loss: {running_loss/print_every:.3f}.. "
+                    f"Test loss: {test_loss/len(val_load):.3f}.. "
+                    f"Test accuracy: {accuracy/len(val_load):.3f}")
+                running_loss = 0
+                model.train()
+
+    # Saving the model with the model name
+    torch.save(model.state_dict(), f'{model_name}.pth')
 
 
-def train(img_path, model):
-    pass
+    
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    # Add arguments for command line
+    args = parser.parse_args()
+
+    return args
+
+
+
+
+def main():
+    args = parse_args()
+
+   
+
+if __name__ == '__main__':
+    main()
+
+
+
+# Set directory to save checkpoints
+# Choose a specific directory to perform Training on
+# Create a function to obtain the path of the input directory
+# Check the format of the images
+# If scattered create labels and group to pass into ImageFolder easily
+
+# Check the imports from data
