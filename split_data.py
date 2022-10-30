608a932e194f1d12262ba44c512e6d6721d761b9
Austin Kaburia <88529649+kaburia@users.noreply.github.com>
2022-10-30 13:39:28 +0300

    Update split_data.py

--- split_data.py
+++ split_data.py
@@ -15,7 +15,7 @@ def split_data(path):
     for meth in method:
         for label in labels:
              # creating a  folder if they do not exist
-            if os.path.isdir(label):
+            
                 if os.path.exists(f'{meth}') is False:
                     os.mkdir(f'{meth}')
                     if os.path.exists(f'{meth}/{label}') is False:
