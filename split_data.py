ff12421489c3a5f73e60d2538fb09529c9ac1e7c
Austin Kaburia <88529649+kaburia@users.noreply.github.com>
2022-11-04 14:42:32 +0300

    Update split_data.py

--- split_data.py
+++ split_data.py
@@ -11,6 +11,7 @@ from random import sample
 # Building and splitting the dataset
 def split_data(path):
     labels = [label for label in listdir(path) if os.path.isdir(label)]
+    print(labels)
     method = ['Train','Validate', 'Test']
     for meth in method:
         for label in labels:
@@ -24,20 +25,20 @@ def split_data(path):
                     os.mkdir(f'{meth}/{label}')
 
                 if meth == 'Train':
-                    length = int(len(listdir(label)) * 0.8) # choosing 80% of the values
-                    sample_list = sample(listdir(label), length)
+                    length = int(len(listdir(path+label)) * 0.8) # choosing 80% of the values
+                    sample_list = sample(listdir(path+label), length)
                     for file in sample_list:
-                        shutil.move(f'{label}/{file}', f'{meth}/{label}') 
+                        shutil.copy(f'{path+label}/{file}', f'{meth}/{label}') 
                 elif meth == 'Validate':
-                    length = int(len(listdir(label)) * 0.5) # choosing 10% of the values
-                    sample_list = sample(listdir(label), length)
+                    length = int(len(listdir(path+label)) * 0.5) # choosing 10% of the values
+                    sample_list = sample(listdir(path+label), length)
                     for file in sample_list:
-                        shutil.move(f'{label}/{file}', f'{meth}/{label}') 
+                        shutil.copy(f'{path+label}/{file}', f'{meth}/{label}') 
                 else:
-                    length = int(len(listdir(label))) # choosing the remaining values for test
-                    sample_list = sample(listdir(label), length)
+                    length = int(len(listdir(path+label))) # choosing the remaining values for test
+                    sample_list = sample(listdir(path+label), length)
                     for file in sample_list:
-                        shutil.move(f'{label}/{file}', f'{meth}/{label}') 
+                        shutil.copy(f'{path+label}/{file}', f'{meth}/{label}') 
     
 
 
