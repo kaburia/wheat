677f1efb5081742f549e62754b046574f8b9d07f
Austin Kaburia <88529649+kaburia@users.noreply.github.com>
2022-11-04 14:45:16 +0300

    Update split_data.py

--- split_data.py
+++ split_data.py
@@ -28,17 +28,17 @@ def split_data(path):
                     length = int(len(listdir(path+label)) * 0.8) # choosing 80% of the values
                     sample_list = sample(listdir(path+label), length)
                     for file in sample_list:
-                        shutil.copy(f'{path+label}/{file}', f'{meth}/{label}') 
+                        shutil.move(f'{path+label}/{file}', f'{meth}/{label}') 
                 elif meth == 'Validate':
                     length = int(len(listdir(path+label)) * 0.5) # choosing 10% of the values
                     sample_list = sample(listdir(path+label), length)
                     for file in sample_list:
-                        shutil.copy(f'{path+label}/{file}', f'{meth}/{label}') 
+                        shutil.move(f'{path+label}/{file}', f'{meth}/{label}') 
                 else:
                     length = int(len(listdir(path+label))) # choosing the remaining values for test
                     sample_list = sample(listdir(path+label), length)
                     for file in sample_list:
-                        shutil.copy(f'{path+label}/{file}', f'{meth}/{label}') 
+                        shutil.move(f'{path+label}/{file}', f'{meth}/{label}') 
     
 
 
